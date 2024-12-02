02bc6711efc3c4ac94fb2fe6b5e2dd8a5976df8f
julia varga <varga_jh@yahoo.com>
2024-12-02 13:06:24 +0200

    Add project files.

---
+++ Tema1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello,");
+
+Console.WriteLine("Please input your First Name: ");
+string FirstName = Console.ReadLine();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Please input your Last Name: ");
+string LastName = Console.ReadLine();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Please input your Age: ");
+int Age = int.Parse(Console.ReadLine());
+
+int CalculateAge = Age + 20;
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Please input your Gender: ");
+string Gender = Console.ReadLine();
+
+
+Console.WriteLine("\n"); 
+
+Console.WriteLine("Your details are as follows: ");
+Console.WriteLine("- First name: " + FirstName);
+Console.WriteLine("- Last name: " + LastName);
+Console.WriteLine("- Gender: " + Gender);
+Console.WriteLine("You will be " +CalculateAge + " years old in 20 years");
